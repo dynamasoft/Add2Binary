9f5fabc42add7b3296bd4b2c96620fd64601a20c
dynamasoft <smithtanny@gmail.com>
2022-03-14 14:11:11 -0400

    Add project files.

---
+++ Add2Binary/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Add2Binary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var result = AddBinary("1010","1011");
+            Console.Write(result);
+        }
+
+
+        static public string AddBinary(string a, string b)
+        {
+            var one= Convert.ToInt32(a, 2);
+            var two= Convert.ToInt32(b, 2);
+
+            var result = Convert.ToString(one + two, 2);
+            return result;
+        }
+    }
+}
